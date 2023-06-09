b95ae224062c520aac697ecea37f7a15df03333d
xiong.chen.ur <cxgoodjob123@gmail.com>
2023-06-09 11:12:45 -0400

    add Config

---
+++ src/main/java/com/ticket/seckill/config/AccessLimit.java
@@ -0,0 +1,22 @@
+package com.ticket.seckill.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * @Author: Shawn Chen
+ * @Date: 2023/6/9
+ **/
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AccessLimit {
+
+    int second();
+
+    int maxCount();
+
+    boolean needLogin() default true;
+}
