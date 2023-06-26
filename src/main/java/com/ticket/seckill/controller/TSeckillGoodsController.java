9d0e7efc5355f743de3c7ebbf831b49763dfd518
xiong.chen.ur <cxgoodjob123@gmail.com>
2023-06-26 17:11:09 -0400

    implement Order, SeckillGoods, SeckillOrder, TUser controllers

---
+++ src/main/java/com/ticket/seckill/controller/TSeckillGoodsController.java
@@ -0,0 +1,18 @@
+package com.ticket.seckill.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 秒杀商品表 前端控制器
+ *
+ * @author LiChao
+ * @since 2022-03-03
+ */
+@RestController
+@RequestMapping("/t-seckill-goods")
+@Api(value = "秒杀商品表", tags = "秒杀商品表")
+public class TSeckillGoodsController {
+
+}
