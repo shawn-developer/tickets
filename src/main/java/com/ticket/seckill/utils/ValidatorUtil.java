47b5c509945ffba01015c623c468fda62cd89852
xiong.chen.ur <cxgoodjob123@gmail.com>
2023-07-28 09:20:16 -0400

    add ValidatorUtil for phone number

---
+++ src/main/java/com/ticket/seckill/utils/ValidatorUtil.java
@@ -0,0 +1,30 @@
+package com.ticket.seckill.utils;
+
+import org.thymeleaf.util.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 手机号码校验类
+ *
+ * @Author: Shawn Chen
+ * @Date: 2023/7/28
+ */
+public class ValidatorUtil {
+
+    private static final Pattern mobile_patten = Pattern.compile("[1]([3-9])[0-9]{9}$");
+
+    /**
+     * 手机号码校验
+     * @param mobile
+     * @return boolean
+     **/
+    public static boolean isMobile(String mobile) {
+        if (StringUtils.isEmpty(mobile)) {
+            return false;
+        }
+        Matcher matcher = mobile_patten.matcher(mobile);
+        return matcher.matches();
+    }
+}
